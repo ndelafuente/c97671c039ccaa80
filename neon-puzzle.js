83b900f4bfb94ba084b21ba682096b8ed2b54e37
Nico de la Fuente <ndelafuente@bionanogenomics.com>
2026-02-26 10:57:20 -0800

    add response logic

--- neon-puzzle.js
+++ neon-puzzle.js
@@ -1,32 +1,62 @@
 function receiveNext() {
     let socket = new WebSocket("wss://neonhealth.software/agent-puzzle/challenge");
     socket.onopen = () => { console.log("Connected") };
-    socket.onmessage = (event) => { handleMessage(event.data) };
+    socket.onmessage = (event) => { handleMessage(socket, event.data) };
     socket.onerror = (err) => { console.error("WebSocket error:", err) };
     socket.onclose = (event) => { console.log("Closed:", event.code, event.reason) };
 }
 
 /**
  * Handle a message from NEON
+ * @param {WebSocket} socket The web socket
  * @param {string} message Expected to be a stringified JSON object
  */
-function handleMessage(message) {
+function handleMessage(socket, message) {
     const parsedMessage = JSON.parse(message)
     console.log("Parsed message", parsedMessage);
 
     switch (parsedMessage['type']) {
         case "challenge":
-            return handleChallenge(parsedMessage['message']);
+            handleChallenge(socket, parsedMessage['message']);
+            return;
+        case "success":
+            console.log("Success!");
+            return;
+        case "error":
+            throw new Error(parsedMessage['message']);
         default: throw new EvalError(`Recieved unknown message type ${type}`);
     }
 }
 
 /**
  * Handle a challenge from NEON
+ * @param {WebSocket} socket The web socket
  * @param {Object[]} fragments a scrambled list of timestamped words
  */
-function handleChallenge(fragments) {
+function handleChallenge(socket, fragments) {
     fragments.sort((a, b) => a['timestamp'] - b['timestamp']);
     const unscrambled = fragments.map(fragment => fragment['word']).join(' ');
     console.log("Unscrambled:", unscrambled);
+}
+
+/**
+ * A robust messenger, accomodating NEON's archaic logic
+ * @param {WebSocket} socket The web socket
+ * @param {string|number} message The message to send
+ * @param {{includePound: boolean}} options
+ */
+function sendResponse(socket, message, { includePound = false } = {}) {
+    const send = (o) => socket.send(JSON.stringify(o));
+    switch (typeof message) {
+        case "number":
+            let digits = message.toString();
+            if (includePound) digits += '#';
+            send({ "type": "enter_digits", digits });
+            return;
+        case "string":
+            // TODO: verify text length?
+            let text = message;
+            send({ "type": "speak_text", text });
+            return;
+    }
 }
